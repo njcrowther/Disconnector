6a95e94bcb728620add72343e2608edd1bdbca60
Nathan Crowther <njcrowther@gmail.com>
2019-05-30 14:27:49 -0500

    verion # alignment

--- disconnector.sketchplugin/Contents/Sketch/disconnector.js
+++ disconnector.sketchplugin/Contents/Sketch/disconnector.js
@@ -1,1 +1,183 @@
-var globalThis=this;function __skpm_run(e,t){globalThis.context=t;var n=function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t);var r=n(1);function o(e){e.layers.forEach(function(e){"Group"===e.type?o(e):"SymbolInstance"===e.type&&u(e)})}function u(e){e.detach({recursively:!0})}t.default=function(){var e=r.getSelectedDocument().selectedLayers,t=e.length;e.forEach(function(e){"Artboard"===e.type?e.layers.forEach(function(e){"Group"===e.type?o(e):"SymbolInstance"===e.type&&u(e)}):"Group"===e.type?o(e):"SymbolInstance"===e.type&&u(e)}),0===t?r.UI.message("Select some objects"):r.UI.message("Symbols Detatched!")}},function(e,t){e.exports=require("sketch")}]);"default"===e&&"function"==typeof n?n(t):n[e](t)}globalThis.onRun=__skpm_run.bind(this,"default");
+var globalThis = this;
+function __skpm_run (key, context) {
+  globalThis.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/disconnector.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/disconnector.js":
+/*!*****************************!*\
+  !*** ./src/disconnector.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var sketch = __webpack_require__(/*! sketch */ "sketch"); // documentation: https://developer.sketchapp.com/reference/api/
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  var doc = sketch.getSelectedDocument(); //Gets the sketch document
+
+  var selection = doc.selectedLayers; // Gets the currently selected artboards or layers
+
+  var selectedCount = selection.length; // Variable I'm using just to log things and make sure it's selecting everything.
+  // function that handles artboards, groups, symbol
+
+  selection.forEach(function (selected) {
+    if (selected.type === 'Artboard') {
+      //get array of layers. send to detatcher
+      artboardDetatcher(selected);
+    } else if (selected.type === 'Group') {
+      // for each object. send to detatcher
+      groupDetatcher(selected);
+    } else if (selected.type === 'SymbolInstance') {
+      detatcher(selected);
+    }
+  });
+
+  if (selectedCount === 0) {
+    sketch.UI.message('Select some objects');
+  } else {
+    sketch.UI.message("Symbols Detatched!");
+  }
+}); // Function to handle a selected artboard
+
+function artboardDetatcher(artboard) {
+  artboard.layers.forEach(function (layer) {
+    if (layer.type === 'Group') {
+      // send group to group function
+      groupDetatcher(layer);
+    } else if (layer.type === 'SymbolInstance') {
+      detatcher(layer);
+    }
+  });
+}
+
+function groupDetatcher(group) {
+  group.layers.forEach(function (layer) {
+    if (layer.type === 'Group') {
+      groupDetatcher(layer);
+    } else if (layer.type === 'SymbolInstance') {
+      detatcher(layer);
+    }
+  });
+} // function to do the detatching
+
+
+function detatcher(layer) {
+  layer.detach({
+    recursively: true
+  });
+}
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+globalThis['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=disconnector.js.map
